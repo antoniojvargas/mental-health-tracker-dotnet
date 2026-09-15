92ec23166425426c3b1fe2d158f5745ef1a5608a
Antonio Vargas <toyoyo600@gmail.com>
2026-09-15 10:41:13 -0500

    feat(daily-log): validate logDate format and reject future dates

--- backend/MentalHealthTracker.Api/Modules/DailyLog/Validators/CreateDailyLogValidator.cs
+++ backend/MentalHealthTracker.Api/Modules/DailyLog/Validators/CreateDailyLogValidator.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Text.RegularExpressions;
 using FluentValidation;
 using MentalHealthTracker.Api.Modules.DailyLog.Dtos;
 
@@ -5,8 +7,18 @@ namespace MentalHealthTracker.Api.Modules.DailyLog.Validators;
 
 public sealed class CreateDailyLogValidator : AbstractValidator<CreateDailyLogRequest>
 {
+    private static readonly Regex DateOnlyFormatRegex = new(@"^\d{4}-\d{2}-\d{2}$", RegexOptions.Compiled);
+
     public CreateDailyLogValidator()
     {
+        RuleFor(x => x.LogDate)
+            .Must(x => DateOnlyFormatRegex.IsMatch(x.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)))
+            .WithMessage("logDate debe tener formato YYYY-MM-DD.");
+
+        RuleFor(x => x.LogDate)
+            .Must(x => x <= DateOnly.FromDateTime(DateTime.UtcNow))
+            .WithMessage("logDate no puede estar en el futuro.");
+
         RuleFor(x => x.MoodRating).InclusiveBetween(1, 5);
 
         RuleFor(x => x.AnxietyLevel).InclusiveBetween(1, 10);
